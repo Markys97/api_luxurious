20abea757f9eac0dc59984a447baa33ae9c8edd1
brexe junior <brexejunior@gmail.com>
2023-05-01 23:30:10 +0300

    send mail

--- index.js
+++ index.js
@@ -2,9 +2,35 @@ const express = require('express')
 var cors = require('cors')
 const mysql= require('mysql');
 const app = express();
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
+
 require('dotenv').config()
 
 const port = process.env.PORT||3500;
+
+const  sendEmail = async (myMail,MyEmailPassword,to,subject,message)=>{
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: myMail,
+      pass: MyEmailPassword
+    }
+  });
+
+  const mailOptions = {
+    from:myMail,
+    to: to,
+    subject: subject,
+    text: message,
+    html:message
+  };
+   let info = await transporter.sendMail(mailOptions);
+   return info
+}
+
+
 
 
 
@@ -30,6 +56,9 @@ connection.connect(function(err) {
 // middleware
 app.use(cors())
 app.use(express.static(__dirname+'/public'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
 
 // route
 app.get('/', (req, res) => {
@@ -87,6 +116,28 @@ app.get('/product/:id',(req,res)=>{
   
 
  
+})
+
+app.post('/user/confirm-email',(req,res)=>{
+  let data = req.body.body;
+  let currentLang = req.body.header.lang
+  console.log(currentLang)
+  data = JSON.parse(data);
+  const userEmail= data.email;
+
+ let token = Date.now().toString().slice(9,13)
+
+  const emailRegex = new RegExp(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/, "gm");
+
+  if(!emailRegex.test(userEmail))  res.status(400).send('bad email')
+
+  sendEmail(process.env.EMAIL,process.env.EMAIL_PASSWORD,process.env.EMAIL,'juste a test ',`ton code est <b>${token}</b>`)
+  .then(data => {
+    if(data.messageId){
+      res.status(200).json({"code":`${token}`})
+    }
+  })
+  
 })
 
 
